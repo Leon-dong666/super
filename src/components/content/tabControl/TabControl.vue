dc75a876234039266a4a17607c3261b5820fcf55
Leon Dong <201921016974@mail.scut.edu.cn>
2020-09-01 11:13:40 +0800

    第二次更新

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" class="tab-control-item">
+      <span  :class="{isActive:currentIndex===index}" @click="btnClick(index)" class="title-item">{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  data() {
+    return{
+      currentIndex:0,
+      isActive: true
+    
+    }
+  },
+  methods: { 
+    btnClick(index){
+      this.currentIndex=index
+    }
+   }
+
+}
+</script>
+
+<style scoped>
+  .tab-control{
+    display: flex;
+    text-align: center;
+    height: 36px;
+    line-height: 36px;
+  }
+  .tab-control-item{
+    flex: 1;
+    
+  }
+  .title-item{
+    padding: 5px;
+  }
+  .isActive{
+    color: var(--color-high-text);
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,15 +5,55 @@
     </navi-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NaviBar from "components/common/navibar/NaviBar";
-import {getHomeMultidata} from 'network/home';
 import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
 
 import HomeSwiper from './childComps/HomeSwiper'
+
+import TabControl from 'components/content/tabControl/TabControl'
+
+import {getHomeMultidata, getHomeGoods} from 'network/home';
 
 export default {
   name: "home",
@@ -21,12 +61,19 @@ export default {
     NaviBar,
     HomeSwiper,
     RecommendView,
+    FeatureView,
+    TabControl,
   },
   data(){
     return{
      // result:null
      banners:[],
-     recommends:[]
+     recommends:[],
+     goods:{
+       'pop':{psge:0,list:[]},
+       'new':{psge:0,list:[]},
+       'sell':{psge:0,list:[]},
+     }
     }
   },
   //函数调用-》压入函数栈（保存函数调用过程中所有变量），临时的
@@ -39,6 +86,9 @@ export default {
       //this.result = res;
       this.banners=res.data.banner.list;
       this.recommends=res.data.recommend.list;
+    }),
+    getHomeGoods('pop',1).then(res=>{
+      console.log(res);
     })
   }
 
@@ -47,8 +97,22 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+  padding-bottom: 49px;
+}
 .nav-home {
   background-color: var(--color-tint);
   color: white;
+  position: fixed;
+  left: 0;
+  right:0 ;
+  top: 0;
+  z-index: 9;
+}
+.tab-control{
+  position: sticky;
+  top: 44px;
+  background-color: #fff;
 }
 </style>
